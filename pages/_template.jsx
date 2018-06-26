166aa1d747ccb0e7a43020fb7f467a79c155996f
Mathias Maisberger <hiasinho@me.com>
2018-06-26 07:18:48 +0200

    Add DSVGO

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -17,12 +17,14 @@ module.exports = React.createClass({
       <div>
         <header className={styles.header}>
           <h1>Fugi-Fix - Pflasterfugenmörtel</h1>
-          <Link to={prefixLink('/')}><img src={prefixLink('/logo.svg')} className={styles.logo}/></Link>
+          <Link to={prefixLink('/')}>
+            <img src={prefixLink('/logo.svg')} className={styles.logo} />
+          </Link>
           <a href="https://www.facebook.com/fugifix" target="_blank" className={styles.facebook}>
-            <MediaQuery query='(min-width: 48em)'>
+            <MediaQuery query="(min-width: 48em)">
               <img src={prefixLink('/images/facebook.svg')} />
             </MediaQuery>
-            <MediaQuery query='(max-width: 767px)'>
+            <MediaQuery query="(max-width: 767px)">
               <img src={prefixLink('/images/facebook.svg')} />
             </MediaQuery>
           </a>
@@ -30,7 +32,7 @@ module.exports = React.createClass({
         {this.props.children}
         <footer className={styles.footer}>
           <Link to={prefixLink('/')}>&copy; fugi-fix.de</Link>
-          <Link to={prefixLink('/impressum/')}>Impressum</Link>
+          <Link to={prefixLink('/impressum/')}>Impressum & Datenschutz</Link>
           <Link to={prefixLink('/agbs/')}>AGBs</Link>
         </footer>
       </div>
