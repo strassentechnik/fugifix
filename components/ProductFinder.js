a9856b875e3d788d3a5a850c4d5fa0cd8ee56064
philippmeissner <pm@zeit.io>
2020-04-03 12:13:26 +0200

    lintfix

--- components/ProductFinder.js
+++ components/ProductFinder.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { prefixLink } from 'gatsby-helpers'
-import content from '../content.yaml'
-import RangeSlider from './RangeSlider'
-import styles from './ProductFinder.module.scss'
-import classNames from 'classnames'
-import numeral from 'numeral'
+import React from 'react';
+import { prefixLink } from 'gatsby-helpers';
+import content from '../content.yaml';
+import RangeSlider from './RangeSlider';
+import styles from './ProductFinder.module.scss';
+import classNames from 'classnames';
+import numeral from 'numeral';
 
 const language = {
   delimiters: {
@@ -20,19 +20,19 @@ const language = {
   currency: {
     symbol: '€',
   },
-}
+};
 
-numeral.language('de', language)
+numeral.language('de', language);
 
-numeral.language('de')
+numeral.language('de');
 
 export default class ProductFinder extends React.Component {
   static propTypes = {
     product: React.PropTypes.number,
-  }
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       weight: 1,
       width: 1,
@@ -42,7 +42,7 @@ export default class ProductFinder extends React.Component {
         width: 0,
         height: 0,
       },
-    }
+    };
 
     this.category = {
       // Human
@@ -73,7 +73,7 @@ export default class ProductFinder extends React.Component {
         max: 50,
         products: [6],
       },
-    }
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -84,31 +84,31 @@ export default class ProductFinder extends React.Component {
       3: 2, // farbig
       5: 3, // schnellfest
       6: 4, // hoch belastbar
-    }
-    const productId = nextProps.product
+    };
+    const productId = nextProps.product;
 
     if (productId != null) {
-      this.state.product = productId
-      this.state.weight = productWeights[productId]
+      this.state.product = productId;
+      this.state.weight = productWeights[productId];
 
       switch (productId) {
         case 0:
-          this.category[1].products = [0]
-          this.state.width = 1
-          break
+          this.category[1].products = [0];
+          this.state.width = 1;
+          break;
 
         case 1:
         case 2:
-          this.category[1].products = [0, 1, 2]
-          this.state.width = this.category[this.state.weight].min > 3 ? this.category[this.state.weight].min : 3
-          this.state.depth = 30
-          break
+          this.category[1].products = [0, 1, 2];
+          this.state.width = this.category[this.state.weight].min > 3 ? this.category[this.state.weight].min : 3;
+          this.state.depth = 30;
+          break;
 
         default:
-          this.category[1].products = [0, 1, 2]
-          this.state.width = this.category[this.state.weight].min > 3 ? this.category[this.state.weight].min : 3
-          this.state.depth = content.products[productId].depth.min
-          break
+          this.category[1].products = [0, 1, 2];
+          this.state.width = this.category[this.state.weight].min > 3 ? this.category[this.state.weight].min : 3;
+          this.state.depth = content.products[productId].depth.min;
+          break;
       }
     }
   }
@@ -118,55 +118,55 @@ export default class ProductFinder extends React.Component {
       weight: weight,
       product: null,
       width: (this.state.width < this.category[weight].min ? this.category[weight].min : this.state.width),
-    })
-  }
+    });
+  };
 
   handleWidthChange = (width) => {
     if (this.state.weight === 1 && width < 4) {
-      this.category[1].products = [0]
-      if (width === 3) this.category[1].products.push(1, 2)
+      this.category[1].products = [0];
+      if (width === 3) this.category[1].products.push(1, 2);
     } else {
-      this.category[1].products = [1, 2]
+      this.category[1].products = [1, 2];
     }
 
     this.setState({
       width: width,
-    })
-  }
+    });
+  };
 
   handleDepthChange = (depth) => {
     this.setState({
       depth: depth,
-    })
-  }
+    });
+  };
 
   handleProductSelect = (product) => {
-    let depth
+    let depth;
 
     switch (product) {
       case 1:
-        depth = 30
-        break
+        depth = 30;
+        break;
 
       case 2:
-        depth = 30
-        break
+        depth = 30;
+        break;
 
       // hoch belastbar
       case 6:
-        depth = this.product(product).depth.min
-        break
+        depth = this.product(product).depth.min;
+        break;
 
       default:
-        depth = 30
-        break
+        depth = 30;
+        break;
     }
 
     this.setState({
       product: product,
       depth: (this.state.depth < depth ? depth : this.state.depth),
-    })
-  }
+    });
+  };
 
   handlePlasterWidthChange = (ev) => {
     this.setState({
@@ -174,8 +174,8 @@ export default class ProductFinder extends React.Component {
         width: parseInt(ev.currentTarget.value),
         height: this.state.plaster.height,
       },
-    })
-  }
+    });
+  };
 
   handlePlasterHeightChange = (ev) => {
     this.setState({
@@ -183,30 +183,30 @@ export default class ProductFinder extends React.Component {
         width: this.state.plaster.width,
         height: parseInt(ev.currentTarget.value),
       },
-    })
-  }
+    });
+  };
 
   product(id) {
-    return content.products[id]
+    return content.products[id];
   }
 
   getResult() {
-    if (this.state.product === null) return 0
+    if (this.state.product === null) return 0;
 
-    const density = this.product(this.state.product).density
-    const { plaster, width, depth } = this.state
-    let result = 0
+    const density = this.product(this.state.product).density;
+    const { plaster, width, depth } = this.state;
+    let result = 0;
 
-    const step1 = (100 / (plaster.width + (width / 10))) + (100 / (plaster.height + (width / 10)))
-    if (!step1) return 0
+    const step1 = (100 / (plaster.width + (width / 10))) + (100 / (plaster.height + (width / 10)));
+    if (!step1) return 0;
 
-    const volume = 100 * (width / 10) * (depth / 10)
-    const mass = volume * density
+    const volume = 100 * (width / 10) * (depth / 10);
+    const mass = volume * density;
 
-    result = ((mass * step1) / 1000)
-    if (!result) return 0
+    result = ((mass * step1) / 1000);
+    if (!result) return 0;
 
-    return result
+    return result;
   }
 
   render() {
@@ -256,34 +256,34 @@ export default class ProductFinder extends React.Component {
         {this.renderSecondPart()}
         {this.renderResult()}
       </div>
-    )
+    );
   }
 
   renderProducts() {
     const products = this.category[this.state.weight].products.map(i => {
       const classes = classNames(styles.finder__products__column, {
         [`${styles['finder__products__column--active']}`]: this.state.product === i,
-      })
+      });
       return (
         <div key={i} className={classes} onClick={this.handleProductSelect.bind(this, i)}>
           <img src={prefixLink(content.products[i].image)}/>
           <div>{content.products[i].title}</div>
         </div>
-      )
-    })
+      );
+    });
 
     return (
       <div>{products}</div>
-    )
+    );
   }
 
   renderResult() {
-    if (this.getResult() === 0) return
+    if (this.getResult() === 0) return;
 
-    const product = content.products[this.state.product]
+    const product = content.products[this.state.product];
     const result = numeral(this.getResult())
-    .format('0,0.0')
-    const mailto = `mailto:info@fugi-fix.de?subject=Angebot für ${product.title}&body=Bitte schicken Sie mir ein Angebot über ${result} kg/m² für das Produkt ${product.title}. Vielen Dank!`
+    .format('0,0.0');
+    const mailto = `mailto:info@fugi-fix.de?subject=Angebot für ${product.title}&body=Bitte schicken Sie mir ein Angebot über ${result} kg/m² für das Produkt ${product.title}. Vielen Dank!`;
 
     return (
       <div>
@@ -307,13 +307,13 @@ export default class ProductFinder extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderSecondPart() {
-    if (this.state.product === null) return
+    if (this.state.product === null) return;
 
-    const { depth } = this.product(this.state.product)
+    const { depth } = this.product(this.state.product);
 
     return (
       <div className={styles.finder__select}>
@@ -344,6 +344,6 @@ export default class ProductFinder extends React.Component {
                  onChange={this.handlePlasterHeightChange}/>
         </div>
       </div>
-    )
+    );
   }
 }
